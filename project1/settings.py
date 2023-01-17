2bb1284edceef93c7d1bbd7d7506097feeccb9ad
LabdhPurohit <80207914+LabdhPurohit@users.noreply.github.com>
2023-01-17 22:18:39 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-@#fop&1mkrdpp2hx0w%%fez4-e*&tqnhvu9(%u60$_#&=zplfv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh']
+# ALLOWED_HOSTS = ['.vercel.app', 'now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -119,8 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
